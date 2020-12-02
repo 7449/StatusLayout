2211b1d3b7d6cc304f8a0b4584573c112400b004
@ <yuezhaoyang7449@163.com>
2020-12-02 11:44:24 +0800

    release

--- library/src/main/java/com/android/status/layout/StatusLayout.kt
+++ library/src/main/java/com/android/status/layout/StatusLayout.kt
@@ -6,6 +6,7 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
 
+@Deprecated("@see StatusView")
 class StatusLayout @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
